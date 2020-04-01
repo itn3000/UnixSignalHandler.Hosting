73fe9d337f0ada5908c66c2fdfca1f044743750f
itn3000 <itn3000@gmail.com>
2020-04-01 16:50:27 +0900

    add sample

---
+++ samples/BasicSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Mono.Unix.Native;
+
+namespace BasicSample
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new HostBuilder()
+                .ConfigureServices((ctx, services) => services.AddSignalHandler(new Signum[]{ Signum.SIGUSR1 }, (s, c) =>
+                    {
+                        Console.WriteLine($"{s}");
+                    }))
+                .RunConsoleAsync();
+        }
+    }
+}
