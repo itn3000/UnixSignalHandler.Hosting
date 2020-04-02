f80af11d05d8d4698ec091d1766f5bcd9b0d7bcc
itn3000 <itn3000@gmail.com>
2020-04-02 18:20:31 +0900

    change signal waiting task to thread(WIP)

--- UnixSignalHandler.Hosting/SignalHandlerHostedService.cs
+++ UnixSignalHandler.Hosting/SignalHandlerHostedService.cs
@@ -10,7 +10,7 @@ using System.Linq;
 
 namespace UnixSignalHandler.Hosting
 {
-    class HandlerErrorEventArgs
+    public class HandlerErrorEventArgs
     {
         public Signum Signal { get; }
         public Exception Error { get; }
@@ -20,10 +20,24 @@ namespace UnixSignalHandler.Hosting
             Error = exception;
         }
     }
+    public class SignalSentEventArgs
+    {
+        public Signum Signal { get; }
+        public bool IsTimeout { get; }
+        public SignalSentEventArgs(Signum signal, bool isTimeout)
+        {
+            Signal = signal;
+            IsTimeout = isTimeout;
+        }
+    }
     public class SignalHandlerHostedService : IHostedService
     {
+        public const string WorkerErrorEventName = "worker_error";
         public const string HandlerErrorEventName = "handler_error";
+        public const string OperationCancelledEventName = "operation_cancelled";
+        public const string SignalSentEventName = "signal_sent";
         public const string DiagnosticName = nameof(SignalHandlerHostedService);
+        Thread _signalTask;
         private static readonly DiagnosticListener _diag = new DiagnosticListener(DiagnosticName);
         ValueTask _task;
         Action<Signum, CancellationToken> _action;
@@ -41,21 +55,38 @@ namespace UnixSignalHandler.Hosting
         {
             while (!_cts.IsCancellationRequested)
             {
-                if (await _channel.Reader.WaitToReadAsync(_cts.Token).ConfigureAwait(false))
+                try
                 {
-                    while (_channel.Reader.TryRead(out var signal) && !_cts.IsCancellationRequested)
+                    if (await _channel.Reader.WaitToReadAsync(_cts.Token).ConfigureAwait(false))
                     {
-                        try
+                        while (_channel.Reader.TryRead(out var signal) && !_cts.IsCancellationRequested)
                         {
-                            _action(signal, _cts.Token);
-                        }
-                        catch (Exception e)
-                        {
-                            if (_diag.IsEnabled(HandlerErrorEventName))
+                            try
                             {
-                                _diag.Write(HandlerErrorEventName, new HandlerErrorEventArgs(signal, e));
+                                _action(signal, _cts.Token);
+                            }
+                            catch (Exception e)
+                            {
+                                if (_diag.IsEnabled(HandlerErrorEventName))
+                                {
+                                    _diag.Write(HandlerErrorEventName, new HandlerErrorEventArgs(signal, e));
+                                }
                             }
                         }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    if (_diag.IsEnabled(OperationCancelledEventName))
+                    {
+                        _diag.Write(OperationCancelledEventName, _signals);
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (_diag.IsEnabled(WorkerErrorEventName))
+                    {
+                        _diag.Write(WorkerErrorEventName, e);
                     }
                 }
             }
@@ -65,14 +96,64 @@ namespace UnixSignalHandler.Hosting
             _cts = new CancellationTokenSource();
             _channel = Channel.CreateUnbounded<Signum>();
             _signalhandle = _signals.Select(s => new UnixSignal(s)).ToArray();
-            _registeredhandle = _signalhandle.Select(s => (s, ThreadPool.RegisterWaitForSingleObject(s,
-                (state, timedOut) =>
+            _signalTask = new Thread(() =>
+            {
+                while (!_cts.IsCancellationRequested)
                 {
-                    if (!timedOut)
+                    try
                     {
-                        _channel.Writer.TryWrite(s.Signum);
+                        var idx = UnixSignal.WaitAny(_signalhandle, TimeSpan.FromSeconds(1));
+                        if (idx >= 0 && idx < _signalhandle.Length)
+                        {
+                            foreach(var s in _signalhandle)
+                            {
+                                if(s.IsSet)
+                                {
+                                    s.Reset();
+                                    _channel.Writer.TryWrite(s.Signum);
+                                }
+                            }
+                        }
                     }
-                }, null, -1, false))).ToArray();
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"signaltask error: {e}");
+                    }
+                }
+            });
+            _signalTask.Start();
+            // _signalTask = Task.WhenAll(_signalhandle.Select(async (s) =>
+            // {
+            //     while(true)
+            //     {
+            //         await Task.Yield();
+            //         try
+            //         {
+            //             if(s.WaitOne(500, false))
+            //             {
+            //                 s.Reset();
+            //                 _channel.Writer.TryWrite(s.Signum);
+            //             }
+            //         }
+            //         catch(Exception e)
+            //         {
+            //             Console.WriteLine($"signaltask error: {e}");
+            //             break;
+            //         }
+            //     }
+            // }));
+            // _registeredhandle = _signalhandle.Select(s => (s, ThreadPool.RegisterWaitForSingleObject(s,
+            //     (state, timedOut) =>
+            //     {
+            //         if(_diag.IsEnabled(SignalSentEventName))
+            //         {
+            //             _diag.Write(SignalSentEventName, new SignalSentEventArgs(s.Signum, timedOut));
+            //         }
+            //         if (!timedOut)
+            //         {
+            //             _channel.Writer.TryWrite(s.Signum);
+            //         }
+            //     }, null, 1000, false))).ToArray();
             _task = Worker();
             return Task.CompletedTask;
         }
@@ -81,16 +162,18 @@ namespace UnixSignalHandler.Hosting
         {
             _cts.Cancel();
             _channel.Writer.Complete();
-            foreach (var (s, r) in _registeredhandle)
-            {
-                r.Unregister(s);
-                s.Close();
-            }
+            // foreach (var (s, r) in _registeredhandle)
+            // {
+            //     r.Unregister(s);
+            //     s.Close();
+            // }
             await _task;
             foreach (var s in _signalhandle)
             {
-                s.Dispose();
+                s.Close();
             }
+            _signalTask.Join();
+            // await _signalTask;
             _cts.Dispose();
         }
     }
--- samples/BasicSample/Program.cs
+++ samples/BasicSample/Program.cs
@@ -3,19 +3,43 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using Mono.Unix.Native;
+using System.Threading;
 
 namespace BasicSample
 {
+    class MyService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    Console.WriteLine("a");
+                    await Task.Delay(10000, stoppingToken);
+                }
+                catch (TaskCanceledException)
+                {
+
+                }
+                catch (OperationCanceledException)
+                {
+
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
 
     class Program
     {
         static async Task Main(string[] args)
         {
             await new HostBuilder()
-                .ConfigureServices((ctx, services) => services.AddSignalHandler(new Signum[]{ Signum.SIGUSR1 }, (s, c) =>
-                    {
-                        Console.WriteLine($"{s}");
-                    }))
+                .ConfigureServices((ctx, services) => services.AddSignalHandler(new Signum[] { Signum.SIGUSR1, Signum.SIGHUP }, (s, c) =>
+                     {
+                         Console.WriteLine($"{s}");
+                     }).AddHostedService<MyService>())
                 .RunConsoleAsync();
         }
     }
